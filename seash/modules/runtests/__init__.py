0b37894fb6ed9f60137c9c483a5d012479553cac
albert <albert.rafetseder+github@univie.ac.at>
2017-02-06 22:40:44 +0100

    Simplify the `runtests` seash module
    Remove the uploaddir functionality, fix up the comments and docstrings,
    and document what we've learned: Don't expect an internal seash function
    to raise an exception! (In our case this means that we would need to
    poll our vessel to see if it has moved to state "Terminated" already
    before we send the next "start" command. Yikes!)

--- seash/modules/runtests/__init__.py
+++ seash/modules/runtests/__init__.py
@@ -5,26 +5,25 @@ import os
 module_help = """
 RunTests Module
 
-This module uploads the contents of a directory of your choice,
-and then runs all of the files as Seattle Unit Tests.
-Don't forget to upload the testrunner beforehand!
+This module runs unit test cases you uploaded to a
+vessel. For this to work, `uploaddir` all of the
+test files you'd like to have run, then upload the
+`testrunner` script, and call the `runtests`
+function to run all of the files contained in the
+tests directory. The vessel log then contains the
+test outcomes and errors.
 
+user@target !> uploaddir /path/to/tests/directory
 user@target !> upload testrunner.r2py
 user@target !> runtests /path/to/tests/directory
-Uploading .....
-Running .....
 user@target !> show log
 """
 
 
 
-def upload_directory_and_run_tests(input_dict, environment_dict):
-  """This function serves to upload every file in a user-supplied 
-  source directory to all of the vessels in the current target group.
-  It essentially calls seash's `upload` function repeatedly, each 
-  time with a file name taken from the source directory.
-  After that, all of the test cases are started on the vessel
-  (one by one).
+def run_tests(input_dict, environment_dict):
+  """Run all the tests in the vessel (one by one), based on the
+  list of files in the directory supplied as our argument.
 
   A note on the input_dict argument:
   `input_dict` contains our own `command_dict` (see below), with 
@@ -69,30 +68,13 @@ your_user_name@browsegood !> runtests a_local_directory
 
   # Sanity check: Is the source dir a directory?
   if not os.path.isdir(source_directory):
-    raise seash_exceptions.UserError("Error: Source directory '" + source_directory + "' is not a directory.\nDid you mean to use the 'upload' command instead?")
+    raise seash_exceptions.UserError("Error: Source directory '" + source_directory + "' is not a directory.")
 
   # Alright --- user input and seash state seem sane, let's do the work!
-  # These are the files we will need to upload:
+  # Iterate over all files; keep trying if the vessel is still running the
+  # previous test; stop on KeyboardInterrupt.
+  # These are the files we will need to run:
   file_list = os.listdir(source_directory)
-
-  # Now upload all the test files
-  for filename in file_list:
-    # We construct the filename-to-be uploaded from the source dir, 
-    # the OS-specific path separator, and the actual file name. 
-    # This is enough for `upload_target` to find the file.
-    path_and_filename = source_directory + os.sep + filename
-    if not os.path.isdir(path_and_filename):
-      print "Uploading '" + path_and_filename + "'..."
-      # Construct an input_dict containing command args for seash's 
-      # `upload FILENAME` function.
-      # XXX There might be a cleaner way to do this.
-      faked_input_dict = {"upload": {"name": "upload", 
-        "children": {path_and_filename: {"name": "filename"}}}}
-      command_callbacks.upload_filename(faked_input_dict, environment_dict)
-    else:
-      print "Skipping sub-directory '" + filename + "'. You may upload it separately."
-
-  # Lastly, run the tests
   file_list.sort()
   for filename in file_list:
     print "Starting '" + filename + "'"
@@ -102,16 +84,29 @@ your_user_name@browsegood !> runtests a_local_directory
     faked_input_dict = {"start": {"name": "start",
       "children": {"testrunner.r2py": {"name": "filename",
       "children": {filename: {"name": "args", "children":{}}}}}}}
-    command_callbacks.start_remotefn_arg(faked_input_dict, environment_dict)
-    
+    try:
+      while True:
+        try:
+          command_callbacks.start_remotefn_arg(faked_input_dict, environment_dict)
+          break
+        except Exception, e:
+          # XXX repyportability meddles with the proper NMClientException
+          # XXX Also, command_callbacks.* never raises exceptions :-(
+          # XXX We'd need to poll the vessel status or similar...
+          if "Vessel has already been started" in str(e):
+            print "Retrying..."
+            sleep(5)
+    except KeyboardInterrupt:
+      break
+
 
 
 
 command_dict = {
   "runtests": {
     "name": "runtests",
-    "callback": upload_directory_and_run_tests,
-    "summary": "Run all tests from the specified directory",
+    "callback": run_tests,
+    "summary": "Run tests from the specified directory",
     "help_text": module_help,
     "children": {
       "[ARGUMENT]": {
